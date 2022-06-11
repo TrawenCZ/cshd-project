63d3cadc9f4ecc37fb49ed49a95b50fdcbaa3823
Marek Skácelík <xskacel@fi.muni.cz>
2022-06-11 21:48:04 +0200

    prettier profile page (css)

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 import fetcher from '../models/fetcher';
 
-import { Layout } from 'antd'
+import { Col, Layout, Row } from 'antd'
 import TextArea from 'antd/lib/input/TextArea';
 import { useParams } from 'react-router-dom';
 import {SmallUserReview} from './SmallUserReview'
@@ -27,14 +27,30 @@ function Profile() {
   return (
     <>
       <Content>
-        <p>{user.username}</p>
-        <img src={user.profilePicture} alt="User's personal icon"/>
-        <TextArea rows={4} value={user.aboutMe} disabled/>
-        {user.reviews.map((review) =>  <SmallUserReview key={review.id} {...review}/>)}
-
+      <Row style={{height:"3em", backgroundColor:"#030d16"}}>
+        <Col span={24}/>
+      </Row>
+        <Row style={{backgroundColor:"#030d16"}}>
+          <Col span={7}/>
+          <Col span={4}>
+            <img style={{objectFit: "cover" , width: "100%", maxHeight: "100%", borderRadius: "50%"}} src={user.profilePicture} alt="User's personal icon"/>
+          </Col>
+          <Col span={2}/>
+          <Col span={8}>
+            <TextArea style={{objectFit: "cover" , width: "100%", height: "100%" ,  resize: "none", fontSize:"1.3em", color:"black"}} value={user.aboutMe} disabled />
+          </Col>
+          <Col span={3}/>
+        </Row>
+      <Row style={{height:"3em", backgroundColor:"#030d16" }}>
+        <Col span={7}/>
+        <Col span={4}>
+            <h1 style={{textAlign:'center', color:"white"}}>{user.username}</h1>
+        </Col>
+        <Col span={13}/>
+      </Row>
+          {user.reviews.map((review, index) =>  (<div style = {(index % 2 === 0) ? {backgroundColor:"#f5f5f5"} : {backgroundColor:"#d9d9d9"}}><SmallUserReview key={review.id} {...review}/></div>))}
       </Content>
     </>
   );
 };
-
 export default Profile;

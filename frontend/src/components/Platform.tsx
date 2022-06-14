7e1ca5e49e95a5cd6a44d5b354dc26fdfa6fcc7c
Marek Skácelík <xskacel@fi.muni.cz>
2022-06-14 22:33:05 +0200

    added a offical webpage button in platforms

--- frontend/src/components/Platform.tsx
+++ frontend/src/components/Platform.tsx
@@ -2,12 +2,13 @@ import {GameProps} from './Game'
 import useSWR from 'swr';
 import fetcher from '../models/fetcher';
 
-import { Col, Layout, Row } from 'antd'
+import { Button, Col, Layout, Row } from 'antd'
 
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import LayoutHeader from './Header';
 import MainFooter from "./MainFooter";
 import SmallGameCard from './SmallGameCard';
+import { HomeOutlined } from '@ant-design/icons';
 
 export interface PlatformProps{
     id:string,
@@ -23,38 +24,45 @@ const { Content } = Layout;
 
 
 function Platform() {
-  const { id } = useParams()
-  const { data, error } = useSWR(`http://localhost:4000/api/platforms/${id}`, fetcher);
+    const { id } = useParams()
+    const { data, error } = useSWR(`http://localhost:4000/api/platforms/${id}`, fetcher);
+    const navigate = useNavigate();
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
 
-
-  const platform: PlatformProps  = data.data;
-  return (
-    <>
-    <Layout>
-      <LayoutHeader/>
-      <Content>
-        <Row style={{height:"3em", backgroundColor:"#030d16"}}>
-            <Col span={24}/>
-        </Row>
-        <Row style={{ backgroundColor:"#030d16"}}>
-            <Col span={5}/>
-            <Col span={14}>
-                <h1 style={{textAlign:"center", color:"white", fontSize:"50px"}}>{platform.name}</h1>
-                <h3 style={{textAlign:"justify", color:"grey"}}>{platform.description}</h3>
-            </Col>
-            <Col span={5}/>
-        </Row>
-        <Row style={{height:"3em", backgroundColor:"#030d16"}}>
-            <Col span={24}/>
-        </Row>
-        {platform.games.map((game, index) =>  (<div style = {(index % 2 === 0) ? {backgroundColor:"#f5f5f5"} : {backgroundColor:"#d9d9d9"}}><SmallGameCard key={game.name} {...game}/></div>))}
-      </Content>
-      <MainFooter/>
-    </Layout>
-    </>
-  );
+    const platform: PlatformProps  = data.data;
+    return (
+        <>
+        <Layout>
+        <LayoutHeader/>
+        <Content>
+        <Row style={{height:"1em", backgroundColor:"#030d16"}}>
+                <Col span={24}/>
+            </Row>
+            <Row style={{height:"2em", backgroundColor:"#030d16"}}>
+                <Col span={21}/>
+                <Col span={2}>
+                    <Button style={{float:"right"}} type="primary" icon={<HomeOutlined />} onClick={() => window.location.replace(platform.officialPage)}>Webpage</Button>
+                </Col>
+                <Col span={1}/>
+            </Row>
+            <Row style={{ backgroundColor:"#030d16"}}>
+                <Col span={5}/>
+                <Col span={14}>
+                    <h1 style={{textAlign:"center", color:"white", fontSize:"50px"}}>{platform.name}</h1>
+                    <h3 style={{textAlign:"justify", color:"grey"}}>{platform.description}</h3>
+                </Col>
+                <Col span={5}/>
+            </Row>
+            <Row style={{height:"3em", backgroundColor:"#030d16"}}>
+                <Col span={24}/>
+            </Row>
+            {platform.games.map((game, index) =>  (<div style = {(index % 2 === 0) ? {backgroundColor:"#f5f5f5"} : {backgroundColor:"#d9d9d9"}}><SmallGameCard key={game.name} {...game}/></div>))}
+        </Content>
+        <MainFooter/>
+        </Layout>
+        </>
+    );
 };
 export default Platform;
